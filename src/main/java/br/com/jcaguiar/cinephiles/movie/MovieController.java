8cf19c6e17443c88c4b5a4855c28ede8cc60dfba
JC-Aguiar <jcostalaguiar@gmail.com>
2022-01-27 10:37:42 -0300

    Testing spyce

--- src/main/java/br/com/jcaguiar/cinephiles/movie/MovieController.java
+++ src/main/java/br/com/jcaguiar/cinephiles/movie/MovieController.java
@@ -11,53 +11,73 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("*/movie")
 public class MovieController {
+    //ATTRIBUTES
+    @Autowired
+    private MovieService service;
 
     @Autowired
-        private MovieService service;
-    @Autowired
-        private ModelMapper modelMapper;
-        private final static ExampleMatcher MATCHER_ALL = ExampleMatcher
+    private ModelMapper modelMapper;
+
+    private final static ExampleMatcher MATCHER_ALL = ExampleMatcher
             .matchingAll().withIgnoreNullValues().withIgnoreCase();
-        private static final Map<String, Method> ENDPOINTS = new HashMap<String, Method>(){{
-            try {
-                put("genre", MovieController.class.getMethod("getGenre", GenreEnum.class));
-                put("title", MovieController.class.getMethod("getTitle", String.class));
-                put("example", MovieController.class.getMethod("getExampleOf", MoviePostRequest.class, int.class, int.class));
-                put("text", MovieController.class.getMethod("getText", String.class));
-            } catch (NoSuchMethodException e) {
-                e.printStackTrace();
-            }
-        }};
 
+    private static final Map<String, Method> ENDPOINTS = new HashMap<String, Method>() {{
+        try {
+            put("genre", MovieController.class.getMethod("getGenre", GenreEnum.class));
+            put("title", MovieController.class.getMethod("getTitle", String.class));
+            put("example", MovieController.class.getMethod("getExampleOf", MoviePostRequest.class, int.class, int.class));
+            put("text", MovieController.class.getMethod("getText", String.class));
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
+    }};
+
+    //CLASS INIT
     {
-
         System.out.println(this.getClass().toString().toUpperCase());
     }
 
     //FIND MOVIES BY GENRE
     @GetMapping(name = "/{var}", params = {"page", "itens"})
-    public ResponseEntity<Page> get
-    (   @PathVariable @Valid GenreEnum genre,
-        @RequestParam(required = false) int page,
-        @RequestParam(required = false) int itens   )
+    public ResponseEntity<?> get(@PathVariable String var,
+                                    @RequestParam(required = false) int page,
+                                    @RequestParam(required = false) int itens)
+    throws InvocationTargetException, IllegalAccessException
     {
-
-        service.getMoviesByGenre(genre);
-        return null;
+        Method methodCall = Arrays.stream(MovieController.class.getMethods())
+                .filter(method -> method.getName().equalsIgnoreCase(var))
+                .findFirst().orElseThrow();
+        itens = itens == 0 ? 12 : itens;
+        final Object[] params = new Object[] { var, page, itens };
+        return (ResponseEntity<?>) methodCall.invoke(this, params);
+//        try {
+//            Optional.of(ENDPOINTS.get(var)).ifPresentOrElse(
+//                    method -> {
+//                        List<Class<?>> classes = Arrays.asList(method.getParameterTypes());
+//                        return (ResponseEntity<Page>) method.invoke(new MovieController(), classes);
+//                    }, () -> {
+//                        throw new RuntimeException("endereço de requisição não encontrado");
+//                    });
+//        } catch (IllegalAccessException | InvocationTargetException e) {
+//                e.printStackTrace();
+//                throw new RuntimeException("erro no decorrer do processo");
+//        }
     }
 
     //FIND MOVIES BY GENRE
     @GetMapping(name = "/{genre}", params = {"page", "itens"})
-    public ResponseEntity<?> getGenre (@PathVariable @Valid GenreEnum genre)
+    public ResponseEntity<?> getGenre (@PathVariable @Valid String genre)
     {
-        service.getMoviesByGenre(genre);
+        GenreEnum genreEnum = Arrays.stream(GenreEnum.values()).filter(
+                en -> genre.equalsIgnoreCase(en.toString())).findFirst().orElseThrow();
+        service.getMoviesByGenre(genreEnum);
         return null;
     }
 
@@ -70,10 +90,9 @@ public class MovieController {
 
     //FIND MOVIES BY ADVANCED SEARCH
     @GetMapping(name = "/{example}", params = {"page", "itens"})
-    public ResponseEntity<Page> getExampleOf
-    (   @PathVariable @Valid MoviePostRequest movie,
-        @RequestParam(required = false) int page,
-        @RequestParam(required = false) int itens   )
+    public ResponseEntity<Page> getExampleOf (@PathVariable @Valid MoviePostRequest movie,
+                                              @RequestParam(required = false) int page,
+                                              @RequestParam(required = false) int itens   )
     {
         itens = itens == 0 ? 12 : itens;
         final Pageable pageResult = PageRequest.of(page, itens, Sort.by("title").ascending());
