2a918e77c07f4dd15d0bc26aac10613fdfd7b3a7
JC-Aguiar <jcostalaguiar@gmail.com>
2022-02-08 13:08:30 -0300

    Adjusting JWT login system (2)

---
+++ src/main/java/br/com/jcaguiar/cinephiles/user/UserController.java
@@ -0,0 +1,49 @@
+package br.com.jcaguiar.cinephiles.user;
+
+import br.com.jcaguiar.cinephiles.util.ConsoleLog;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @ConsoleLog
+    @GetMapping
+    public ResponseEntity<?> test() {
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @ConsoleLog
+    @PostMapping("/test")
+    public ResponseEntity<?> test2() {
+        System.out.println("TESTE");
+        return new ResponseEntity<>(null, HttpStatus.OK);
+    }
+
+    @ConsoleLog
+    @Transactional
+    @PostMapping("/add")
+    public ResponseEntity<?> add(@Valid @RequestBody UserDtoRequest userRequest) {
+        System.out.println("TESTE");
+        final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        userRequest.setPassword( encoder.encode(userRequest.getPassword()) );
+        final UserEntity user = modelMapper.map(userRequest, UserEntity.class);
+        userService.addUser(user);
+        final UserDtoResponse userResponse = modelMapper.map(user, UserDtoResponse.class);
+        return new ResponseEntity<>(userResponse, HttpStatus.OK);
+    }
+}
